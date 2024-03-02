0b356d6d2158d6069a27fae8389b79c8fd3aa01a
Aryan-B <baryan.edu@gmail.com>
2024-03-02 12:58:18 -0800

    frontend basic UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { ChakraBaseProvider } from "@chakra-ui/react";
+// `@chakra-ui/theme` is a part of the base install with `@chakra-ui/react`
+import chakraTheme from "@chakra-ui/theme";
+
+import UploadPage from "./componenets/UploadPage";
+import Forms from "./componenets/Forms";
+import Carousels from "./componenets/Carousels";
+import LandingPage from "./componenets/LandingPage";
+import Navbar from "./componenets/navbar";
+import Topics from "./componenets/Topics";
+// import { set } from "../../server/app";
 
 function App() {
+  const [show, setShow] = useState(true);
+  const [sequence, setSequence] = useState(0);
+  const [pdfId, setPdfId] = useState('');
+  const [questions, setQuestions] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ChakraBaseProvider theme={chakraTheme}>
+        <div>
+          <LandingPage handleShow={setShow} show={show} />
+          {!show ? (
+            <div>
+              <Navbar handleSequence={setSequence}/>
+              {sequence === 0 && <UploadPage handleSequence={setSequence} handlePdfId={setPdfId}/>}
+              {sequence === 1 && <Forms handleSequence={setSequence} pdfId={pdfId} setQuestions={setQuestions}/>}
+              {sequence === 3 && <Topics handleSequence={setSequence}/>}
+              {sequence === 2 && <Carousels questions={questions}/>}
+             
+              
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+      </ChakraBaseProvider>
+    </>
   );
 }
 
