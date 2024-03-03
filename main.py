6d244fef0c82754733a60b8c834d3d20920f3943
Ramit <ramit.kataria@gmail.com>
2024-03-02 18:00:56 -0800

    Fix static build path

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder="build", static_url_path="/")
+app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
 CORS(app)
 #
 session = boto3.Session() #sets the profile name to use for AWS credentials
