41b79a8b831a88ff88a87ff7b7e95d329225b13e
ivan-fediaev <ivanfediaev@hotmail.com>
2024-03-02 16:26:40 -0800

    file upload

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 UPLOAD_FOLDER = "./data"
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+context = ""
 
 # AWS_ACCESS_KEY_ID = "rHGojXd5axr5t1VzTQChE2VyqqZ32UVoI/Vt5SDS"
 # AWS_SECRET_ACCESS_KEY = "rHGojXd5axr5t1VzTQChE2VyqqZ32UVoI/Vt5SDS"
@@ -42,6 +43,33 @@ def upload_file():
     # Save the file to a location on the server
     file.save('./data/' + file.filename)
     
+    with open(f"./data/{file.filename}") as file:
+        context = file.read()
+        print(context)
+        print("working")
+        body = json.dumps({
+            "prompt": f"Please tell me the keywords of these notes and return them as single words answers in a comma delimited list without any numbers and absolutely no other information from the model, just the list: {context}", #AI21
+            "maxTokens": 8191, 
+            "temperature": 0, 
+            "topP": 0.5, 
+            "stopSequences": [], 
+            "countPenalty": {"scale": 0 }, 
+            "presencePenalty": {"scale": 0 }, 
+            "frequencyPenalty": {"scale": 0 }
+        }) #build the request payload
+
+        #
+        response = bedrock.invoke_model(body=body, modelId=bedrock_model_id, accept='application/json', contentType='application/json') #send the payload to Bedrock
+
+        #
+        response_body = json.loads(response.get('body').read()) # read the response
+
+        response_text = response_body.get("completions")[0].get("data").get("text")
+        res_list = response_text.split(",")
+        return res_list
+
+
+
     return 'File uploaded successfully'
 
 @app.route("/hello")
